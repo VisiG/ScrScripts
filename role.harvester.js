66ec5ae1c23773fcc535394d605b46b80ab29e5c
VisiG <mlj1985@gmx.de>
2017-08-16 18:13:24 +0200

    Update role.harvester.js

--- role.harvester.js
+++ role.harvester.js
@@ -104,6 +104,11 @@ var roleHarvester = {
                         }
                 });
                 if(targets.length > 0) {
+					
+					if(doReverse)
+					{
+						targets = targets.reverse();
+					}
                     if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                         creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                     }
