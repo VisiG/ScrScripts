713e468ad7f99a3ff2e25ce4052efd6474db42b4
VisiG <mlj1985@gmx.de>
2017-08-26 08:11:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ module.exports.loop = function () {
     var roomEnergy = currentSpawn.room.energyCapacityAvailable;
     var roomEnergyAvailable = currentSpawn.room.energyAvailable;
     if(harvesters.length < 2) {
-        console.log(harvesters.length + ' ' + roomEnergy);
+        console.log(harvesters.length + ' ' + roomEnergyAvailable);
         if(harvesters.length < 1)
         {
             if(roomEnergyAvailable >= 1800)
